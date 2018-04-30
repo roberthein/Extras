f91f66d2a7082970f262f7da5ae291470f907260
Robert-Hein Hooijmans <rh.hooijmans@gmail.com>
2018-04-30 21:28:17 +0200

    Add some extra comments.

--- Extras/Classes/Extensions/SceneKit/SCNScene.swift
+++ Extras/Classes/Extensions/SceneKit/SCNScene.swift
@@ -3,6 +3,9 @@ import SceneKit
 
 public extension SCNScene {
     
+    /// A initializer for loading a .dae file that's explicitly unwrapped.
+    ///
+    /// - Parameter name: The name of the .dae file without extension.
     public convenience init(daeName name: String) {
         self.init(named: name + ".dae")!
     }
