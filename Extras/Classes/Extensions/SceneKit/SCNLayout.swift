9af097a0b3bcc0185ae69c73db616fce7fb5347b
Robert-Hein Hooijmans <rh.hooijmans@gmail.com>
2019-11-28 12:27:35 +0100

    Small cleanup.

--- Extras/Classes/Extensions/SceneKit/SCNLayout.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-import SceneKit.SceneKitTypes
-
-public enum SCNLayout: String {
-    case horizontal
-    case vertical
-}
--- Extras/Classes/Extensions/SceneKit/SCNMaterial.swift
+++ Extras/Classes/Extensions/SceneKit/SCNMaterial.swift
@@ -3,19 +3,6 @@ import SceneKit
 
 public extension SCNMaterial {
     
-    class func PBR() -> SCNMaterial {
-        let material = SCNMaterial()
-        material.lightingModel = .physicallyBased
-        
-        return material
-    }
-    
-    convenience init(color: UIColor) {
-        self.init()
-        lightingModel = .physicallyBased
-        diffuse.contents = color
-    }
-    
     func apply(_ modifiers: [SCNShaderModifierEntryPoint : String], _ values: [String: Any]) {
         shaderModifiers = modifiers
         values.forEach { setValue($0.value, forKey: $0.key) }
